7f0ffbd5db1e8d846b3dbf3dd384b8e5c476ec2b
yeonwoo-Kim <yeon.8d@gmail.com>
2023-01-16 18:53:26 +0900

    회원가입 API의 email이 중복되어 실패하는 통합 테스트 케이스 작성

--- src/test/java/project/payhereprocess/presentation/user/UserControllerTest.java
+++ src/test/java/project/payhereprocess/presentation/user/UserControllerTest.java
@@ -1,6 +1,7 @@
 package project.payhereprocess.presentation.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.api.AbstractThrowableAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import project.payhereprocess.exception.BusinessException;
 import project.payhereprocess.presentation.user.dto.SignupRequestDto;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -39,6 +42,22 @@ class UserControllerTest {
                         status().isOk(),
                         jsonPath("email").value(notDuplicatedEmailDto.getEmail())
                 );
+    }
+
+    @Test
+    @DisplayName("회원가입 API 실패, email이 중복됨")
+    void signup_fail_duplicated_email() throws Exception {
+        // given
+        SignupRequestDto duplicatedEmailDto = new SignupRequestDto("any@any.com", "password");
+        postRequest("/user/signup", duplicatedEmailDto);
+
+        // when
+        AbstractThrowableAssert<?, ? extends Throwable> expectedException = assertThatThrownBy(
+                () -> postRequest("/user/signup", duplicatedEmailDto)
+        );
+
+        // then
+        expectedException.getCause().isInstanceOf(BusinessException.class);
     }
 
     private ResultActions postRequest(String requestUrl, Object content) throws Exception {
