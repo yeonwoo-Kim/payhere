670de9106d9324b888274275e0671f42d79981a0
yeonwoo-Kim <yeon.8d@gmail.com>
2023-01-16 18:39:14 +0900

    RequestDto 기본 생성자 추가

--- src/main/java/project/payhereprocess/presentation/user/dto/SignupRequestDto.java
+++ src/main/java/project/payhereprocess/presentation/user/dto/SignupRequestDto.java
@@ -1,12 +1,16 @@
 package project.payhereprocess.presentation.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import project.payhereprocess.business.user.command.UserCommand;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignupRequestDto {
     @Email
     @NotNull(message = "이메일을 입력해주세요")
