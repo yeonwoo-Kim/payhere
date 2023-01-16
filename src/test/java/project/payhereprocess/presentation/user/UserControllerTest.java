564b0224f83d1cf4e29831599286226dd0c02f87
yeonwoo-Kim <yeon.8d@gmail.com>
2023-01-16 18:39:35 +0900

    회원가입 API의 성공하는 통합 테스트 케이스 작성

---
+++ src/test/java/project/payhereprocess/presentation/user/UserControllerTest.java
@@ -0,0 +1,53 @@
+package project.payhereprocess.presentation.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import project.payhereprocess.presentation.user.dto.SignupRequestDto;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("회원가입 API 성공")
+    void signup_success() throws Exception {
+        // given
+        SignupRequestDto notDuplicatedEmailDto = new SignupRequestDto("any@any.com", "password");
+
+        // when
+        ResultActions result = postRequest("/user/signup", notDuplicatedEmailDto);
+
+        // then
+        result
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("email").value(notDuplicatedEmailDto.getEmail())
+                );
+    }
+
+    private ResultActions postRequest(String requestUrl, Object content) throws Exception {
+        return mvc.perform(
+                post(requestUrl)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")
+                        .content(objectMapper.writeValueAsString(content))
+        );
+    }
+}
